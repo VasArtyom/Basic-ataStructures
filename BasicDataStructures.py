fb5933b40b99b7e0eb83c6a4b16e0930a8c39456
Vasliaev Artyom <arcryt2122@gmail.com>
2024-11-21 00:09:11 +0300

    Add files via upload

---
+++ BasicDataStructures.py
@@ -0,0 +1,20 @@
+#1st program
+print(9 ** 0.5 * 5)
+
+#2nd program
+print(9.99 > 9.98 and 1000 != 1000.1)
+
+#3rd program
+print(2 * 2 + 2)
+
+print(2 * (2 + 2))
+
+print((2 * 2 + 2) == (2 * (2 + 2)))
+
+#4th program
+data = "123.456"
+stage1 = float(data)
+stage2 = stage1 * 10
+stage3 = stage2 % 10
+stage4 = int(stage3)
+print(stage4)
